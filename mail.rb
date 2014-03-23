40b947bd7419d0221f6c57ec65f63c1f85c71f7c
iguto <clc.iguto@gmail.com>
2014-03-23 16:42:08 +0900

    close #2
    	#2のほかに…
    	Gmail::Messageにメソッドを上書きした。
    	  - read: text,htmlメールにかかわらず、メール本文を取得する
    		- subject: 日本語ととして件名を取得する

--- mail.rb
+++ mail.rb
@@ -1,36 +1,45 @@
 # -*- coding: utf-8 -*-
-# require 'gmail'
+%w(yaml base64 kconv).each do |lib|
+  require lib
+end
+
 require 'bundler'
 Bundler.require
-require 'yaml'
 
 config = YAML.load_file("config.yml")
 username = config["username"]
 password = config["password"]
+#----
+filter = { to: "slp@messiaen.eng.kagawa-u.ac.jp", from: "-amazon"}
+@reg = /^■\s*予定.*/m
+
+module Gmail
+  class Message
+    def read
+      unless multipart?
+        body.to_s.toutf8
+      else
+        text_part.decoded
+      end
+    end
+    def subject
+      super.toutf8
+    end
+  end
+end
 
 # uidでuniqに識別可能？
 #   uidをファイル保存して、
 gmail = Gmail.new(username, password)
 @gmail = gmail
-slp_mails = gmail.inbox.emails(to: "slp@messiaen.eng.kagawa-u.ac.jp")
+
+# slp_mails = gmail.inbox.emails(filter).lazy.select{ |m| m.subject.toutf8.include? "活動" }.select { |m| m.read.include? "予定" }
+slp_mails = gmail.inbox.emails(filter).lazy.select{ |m| m.subject.include? "活動" }.select { |m| m.read.include? "予定" }
+
 p slp_mails_size: slp_mails.size
 
-p slp_mails.first
-p slp_mails.last.uid
 @slp_mails = slp_mails
-
-def read(mail)
-  unless mail.multipart?
-    mail.body.to_s.encode("utf-8", mail.charset)
-  else
-    mail.text_part.decoded
-  end
-end
+@plans = @slp_mails.take(5).to_a.map { |e| e.read.scan(@reg) }
 
 
-module Reader
-  def read(mail)
-    mail.body.to_s.encode("utf-8", mail.charset)
-  end
-  module_function :read
-end
+
